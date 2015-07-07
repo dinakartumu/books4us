e13b1c2ee4d7f33c5fbadef4a7c320ea9d6136d9
Dinakar Tumu <dinakar91@gmail.com>
2015-07-06 22:58:26 -0400

    Database related issue
    Changed names of the database

--- saveProducts.php
+++ saveProducts.php
@@ -1,7 +1,7 @@
 <?php
 //include "connection.php";
 // Create connection
-$conn = mysqli_connect('localhost', 'root', '', 'krishnam_books4us');
+$conn = mysqli_connect('localhost', 'root', '', '');
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
